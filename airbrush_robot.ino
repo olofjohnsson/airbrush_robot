4ae79f1a985bc239d51c4f4f5c2adf4557b26fb2
Olof Johnsson <olof.s.johnsson@gmail.com>
2024-09-13 23:13:43 +0200

    Refactor, clean up and introduce start position

--- airbrush_robot.ino
+++ airbrush_robot.ino
@@ -1,90 +1,90 @@
 #include <stdint.h>
 
-
-#define MOTOR_1_DIR_PIN 2
-#define MOTOR_1_PUL_POS_PIN 3
-#define MOTOR_2_DIR_PIN 4
-#define MOTOR_2_PUL_POS_PIN 5
-#define MOTOR_3_DIR_PIN 9
-#define MOTOR_3_PUL_POS_PIN 11
-#define RAMP_PERIOD_START_MOTOR_1 800
-#define RAMP_PERIOD_END_MOTOR_1 50
-#define RAMP_PERIOD_START_MOTOR_2 900
-#define RAMP_PERIOD_END_MOTOR_2 5
-#define PERIOD_MOTOR_2 20
-#define PERIOD_MOTOR_1 100
-#define PERIOD_MOTOR_3 100
-#define ONE_REV 25000
-#define QUARTER_REV 25000/4
-#define RUN_PULSES PULSE_PER_REV
 #define CONFIG_LED_PIN 13
+
+// Pin Definitions
+#define SIGN_MOTOR_DIR_PIN 2
+#define SIGN_MOTOR_PUL_POS_PIN 3
+#define SWIPE_MOTOR_DIR_PIN 4
+#define SWIPE_MOTOR_PUL_POS_PIN 5
+#define PAINT_MOTOR_DIR_PIN 12
+#define PAINT_MOTOR_PUL_POS_PIN 11
 #define PWR_SW_1 7
 #define PWR_SW_PWR 6
 #define LIMIT_SW_2 8
 #define LIMIT_SW_1 10
 
-#define RAMP_STEP 0.008
+// Timing Constants
+#define PERIOD_SIGN_MOTOR 100
+#define PERIOD_SWIPE_MOTOR 20
+#define PERIOD_SWIPE_MOTOR_INIT 200
+#define PERIOD_PAINT_MOTOR 500
+#define PAINT_BUTTON_STEPS 500
+#define SIGN_ROTATE_STEPS 3000
+#define SWIPE_MOTOR_INIT_STEPS 1500
 
-bool ramp_up = true;
+// Ramping Constants
+#define RAMP_UP_STEP 0.008
+#define RAMP_DWN_STEP 0.02
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(MOTOR_1_DIR_PIN, OUTPUT);
-  pinMode(MOTOR_2_DIR_PIN, OUTPUT);
-  pinMode(MOTOR_3_DIR_PIN, OUTPUT);
-  pinMode(MOTOR_1_PUL_POS_PIN, OUTPUT);
-  pinMode(MOTOR_2_PUL_POS_PIN, OUTPUT);
-  pinMode(MOTOR_3_PUL_POS_PIN, OUTPUT);
+// Enumerations for direction and paint state
+enum swipe_direction { LEFT = true, RIGHT = false };
+enum paint { ON = false, OFF = true };
 
-  pinMode(PWR_SW_1, INPUT_PULLUP);
-  pinMode(PWR_SW_PWR, OUTPUT);
-  pinMode(LIMIT_SW_1, INPUT);
-  pinMode(LIMIT_SW_2, INPUT);
-
-  digitalWrite(MOTOR_1_DIR_PIN, HIGH);
-  digitalWrite(MOTOR_2_DIR_PIN, HIGH);
-  digitalWrite(MOTOR_3_DIR_PIN, HIGH);
-  digitalWrite(MOTOR_1_PUL_POS_PIN, LOW);
-  digitalWrite(MOTOR_2_PUL_POS_PIN, LOW);
-  digitalWrite(MOTOR_3_PUL_POS_PIN, LOW);
-  digitalWrite(PWR_SW_PWR, LOW);
-
+// Motor Direction Control
+void set_motor_direction(uint8_t dir_pin, bool direction) {
+    digitalWrite(dir_pin, direction);
 }
 
-void run_steps_acceleration(int nr_of_steps, long ramp_start, long ramp_end, int pin)
-{
-  long pulse_period = ramp_start;
-  for (int i=0;i<=nr_of_steps;i++)
-  {
-    if (pulse_period > ramp_end && ramp_up)
-    {
-      pulse_period--;
+// Motor Pulse Control
+void pulse_motor(int pin, long pulse_period, int steps) {
+    for (int i = 0; i < steps; i++) {
+        digitalWrite(pin, HIGH);
+        delayMicroseconds(pulse_period);
+        digitalWrite(pin, LOW);
+        delayMicroseconds(pulse_period);
     }
-    if (i>nr_of_steps-(ramp_start-ramp_end))
-    {
-      ramp_up = false;
-      pulse_period++;
-    }
-    digitalWrite(pin, HIGH);
-    delayMicroseconds(pulse_period);
-    digitalWrite(pin, LOW);
-    delayMicroseconds(pulse_period);
-  }
-  ramp_up = true;
 }
 
-void run(int pin, long pulse_period)
-{
-  digitalWrite(pin, HIGH);
-  delayMicroseconds(pulse_period);
-  digitalWrite(pin, LOW);
-  delayMicroseconds(pulse_period);
+// Setup Function
+void setup_pins() {
+    pinMode(SIGN_MOTOR_DIR_PIN, OUTPUT);
+    pinMode(SWIPE_MOTOR_DIR_PIN, OUTPUT);
+    pinMode(PAINT_MOTOR_DIR_PIN, OUTPUT);
+    pinMode(SIGN_MOTOR_PUL_POS_PIN, OUTPUT);
+    pinMode(SWIPE_MOTOR_PUL_POS_PIN, OUTPUT);
+    pinMode(PAINT_MOTOR_PUL_POS_PIN, OUTPUT);
+    
+    pinMode(PWR_SW_PWR, OUTPUT);
+    digitalWrite(PWR_SW_PWR, LOW);
+    pinMode(PWR_SW_1, INPUT_PULLUP);
+    
+    pinMode(LIMIT_SW_1, INPUT);
+    pinMode(LIMIT_SW_2, INPUT);
+
+    digitalWrite(PWR_SW_PWR, LOW);
 }
 
-void run_ramp(int pin, float min_pulse_period, float max_pulse_period)
-{
+// Start and Stop Paint Motor
+void start_paint_motor(int steps, long period) {
+    set_motor_direction(PAINT_MOTOR_DIR_PIN, ON);
+    pulse_motor(PAINT_MOTOR_PUL_POS_PIN, period, steps);
+}
+
+void stop_paint_motor(int steps, long period) {
+    set_motor_direction(PAINT_MOTOR_DIR_PIN, OFF);
+    pulse_motor(PAINT_MOTOR_PUL_POS_PIN, period, steps);
+}
+
+// Rotate Sign Motor
+void rotate_sign_motor(int steps, long period, bool direction) {
+    set_motor_direction(SIGN_MOTOR_DIR_PIN, direction);
+    pulse_motor(SIGN_MOTOR_PUL_POS_PIN, period, steps);
+}
+
+// Ramp-up Function
+void run_ramp(int pin, float min_pulse_period, float max_pulse_period) {
     float pulse_period = max_pulse_period;
-    static bool up = true; 
 
     // Ramp-up phase
     while (pulse_period > min_pulse_period) {
@@ -92,78 +92,80 @@ void run_ramp(int pin, float min_pulse_period, float max_pulse_period)
         delayMicroseconds(pulse_period);
         digitalWrite(pin, LOW);
         delayMicroseconds(pulse_period);
-        
-        // Decrease the pulse period to increase speed (ramp-up)
-        pulse_period -= RAMP_STEP;  // Adjust step size if necessary for smoother ramping
+        pulse_period -= RAMP_UP_STEP;
     }
 
-    pulse_period = min_pulse_period;
-
-    // Continue running at constant speed (min_pulse_period)
+    // Constant speed
     while (true) {
         digitalWrite(pin, HIGH);
         delayMicroseconds(min_pulse_period);
         digitalWrite(pin, LOW);
         delayMicroseconds(min_pulse_period);
-        if (digitalRead(LIMIT_SW_1))
-        {            
-            while (pulse_period < max_pulse_period) 
-            {
-              digitalWrite(pin, HIGH);
-              delayMicroseconds(pulse_period);
-              digitalWrite(pin, LOW);
-              delayMicroseconds(pulse_period);
-        
-            // Decrease the pulse period to increase speed (ramp-up)
-            pulse_period += RAMP_STEP;  // Adjust step size if necessary for smoother ramping
+
+        if (digitalRead(LIMIT_SW_1) || digitalRead(LIMIT_SW_2)) {
+            pulse_period = min_pulse_period;
+            while (pulse_period < max_pulse_period) {
+                digitalWrite(pin, HIGH);
+                delayMicroseconds(pulse_period);
+                digitalWrite(pin, LOW);
+                delayMicroseconds(pulse_period);
+                pulse_period += RAMP_DWN_STEP;
             }
-            delay(100);
-            digitalWrite(MOTOR_2_DIR_PIN, HIGH);
             break;
         }
-        if (digitalRead(LIMIT_SW_2))
+    }
+}
+
+// Main Robot Functionality
+void run_robot() {
+    if (digitalRead(PWR_SW_1)) {
+        run_ramp(SWIPE_MOTOR_PUL_POS_PIN, 18, 38);
+        if (digitalRead(LIMIT_SW_1)) 
         {
-            while (pulse_period < max_pulse_period) 
-            {
-              digitalWrite(pin, HIGH);
-              delayMicroseconds(pulse_period);
-              digitalWrite(pin, LOW);
-              delayMicroseconds(pulse_period);
-        
-            // Decrease the pulse period to increase speed (ramp-up)
-            pulse_period += RAMP_STEP;  // Adjust step size if necessary for smoother ramping
-            }
-            delay(100);
-            digitalWrite(MOTOR_2_DIR_PIN, LOW);
-            break;
+          set_motor_direction(SWIPE_MOTOR_DIR_PIN, LEFT);
+          stop_paint_motor(PAINT_BUTTON_STEPS, PERIOD_PAINT_MOTOR);
+          rotate_sign_motor(SIGN_ROTATE_STEPS, PERIOD_SIGN_MOTOR, true);
+          if (digitalRead(PWR_SW_1))
+          {
+            start_paint_motor(PAINT_BUTTON_STEPS, PERIOD_PAINT_MOTOR);
+          }
         }
+
+        if (digitalRead(LIMIT_SW_2)) {
+          set_motor_direction(SWIPE_MOTOR_DIR_PIN, RIGHT);
+          stop_paint_motor(PAINT_BUTTON_STEPS, PERIOD_PAINT_MOTOR);
+          rotate_sign_motor(SIGN_ROTATE_STEPS, PERIOD_SIGN_MOTOR, true);
+          if (digitalRead(PWR_SW_1))
+          {
+            start_paint_motor(PAINT_BUTTON_STEPS, PERIOD_PAINT_MOTOR);
+          }
+        }
+        
     }
 }
 
-void toggle_motor_direction(uint8_t pin)
-{
-    static bool dir = LOW;
-    digitalWrite(pin, dir);
-    dir = !dir;
+// Move to Start Position
+void go_to_start_position() {
+    if (!digitalRead(LIMIT_SW_2))
+    {
+      set_motor_direction(SWIPE_MOTOR_DIR_PIN, LEFT);
+      run_ramp(SWIPE_MOTOR_PUL_POS_PIN, 18, 38);
+      Serial.println("Init ramp");
+      set_motor_direction(SWIPE_MOTOR_DIR_PIN, RIGHT);
+    }
+    
+    Serial.println("pulse motoer");
+    pulse_motor(SWIPE_MOTOR_PUL_POS_PIN, PERIOD_SWIPE_MOTOR_INIT, SWIPE_MOTOR_INIT_STEPS);
+    while(!digitalRead(PWR_SW_1));
+    start_paint_motor(PAINT_BUTTON_STEPS, PERIOD_PAINT_MOTOR);
 }
 
-void run_air_brush_motor()
-{
-    if (digitalRead(LIMIT_SW_1))
-    {
-        digitalWrite(MOTOR_2_DIR_PIN, HIGH);
-    }
-    if (digitalRead(LIMIT_SW_2))
-    {
-        digitalWrite(MOTOR_2_DIR_PIN, LOW);
-    }
-    if (digitalRead(PWR_SW_1))
-    {
-        run_ramp(MOTOR_2_PUL_POS_PIN, 18, 38);
-    }
+void setup() {
+    Serial.begin(9600);
+    setup_pins();
+    go_to_start_position();
 }
 
-void loop()
-{
-  run_air_brush_motor();
+void loop() {
+    run_robot();
 }
